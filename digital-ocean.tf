0da9e04dec3d446d95f00a97d154c97e0ca9575f
bibaijin <bibaijin@gmail.com>
2017-08-13 10:53:13 +0800

    it works

--- digital-ocean.tf
+++ digital-ocean.tf
@@ -1,20 +1,31 @@
 variable "do_token" {}
+variable "do_ssh_keys" {
+  type = "list"
+}
+variable "do_droplet_id" {}
+variable "do_domain_name" {}
+variable "do_region" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
 resource "digitalocean_droplet" "k8s-node1" {
-  image = "debian-9-x64"
+  image = "ubuntu-16-04-x64"
   name = "k8s-node1"
-  region = "sgp1"
-  size = "512mb"
+  region = "${var.do_region}"
+  size = "1gb"
   ipv6 = true
   private_networking = true
-  ssh_keys = [11847042]
+  ssh_keys = "${var.do_ssh_keys}"
+}
+
+resource "digitalocean_floating_ip" "default" {
+  droplet_id = "${var.do_droplet_id}"
+  region = "${var.do_region}"
 }
 
 resource "digitalocean_domain" "default" {
-  name = "bibaijin.me"
-  ip_address = "${digitalocean_droplet.k8s-node1.ipv4_address"
+  name = "${var.do_domain_name}"
+  ip_address = "${digitalocean_floating_ip.default.ip_address}"
 }
